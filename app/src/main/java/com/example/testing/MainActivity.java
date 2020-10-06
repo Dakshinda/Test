3d7f8942d534c8c99b29e90df16d1b1fe0f5181a
Dakshinda <IT19032320@my.sliit.lk>
2020-10-06 22:34:33 +0530

    Added test2 layout and toast

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -27,7 +30,19 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("n1",edtN1.getText().toString());
         intent.putExtra("n2",edtN2.getText().toString());
 
-        Toast.makeText(this,"Opening Calculator....",Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this,"Opening Calculator....",Toast.LENGTH_SHORT).show();
+        //Creating the LayoutInflater instance
+        LayoutInflater li = getLayoutInflater();
+        //Getting the View object as defined in the customtoast.xml file
+        View layout = li.inflate(R.layout.test2, (ViewGroup)
+                findViewById(R.id.custom_toast_layout));
+        //Creating the Toast object
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setView(layout);//setting the view of custom toast layout
+        toast.show();
+
 
         startActivity(intent);
     }
